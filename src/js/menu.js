599b3360d237753a718c4595f0e02a7af8285c0e
StehCastell <stehcastell@gmail.com>
2022-11-29 01:40:29 -0300

    ajustes no menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,23 +2,23 @@ function menuPaginaAtual(ondeEstou){
     let menuPrincipal = document.getElementById("menuPrincipal");
     let conteudoHTML = "";
     
-    conteudoHTML += `<ul class="nav">`;
+    conteudoHTML += `<ul class="nav ul-menu">`;
 
     if(ondeEstou != "home-page"){
-        conteudoHTML += `<li class="nav-item space-menu"><a class="nav-item" href="home-page.html">Home Page</a></li>`;
+        conteudoHTML += `<li class="nav-item menu-alinhamento"><a class="nav-item" href="home-page.html">Home Page</a></li>`;
     }
 
     if(ondeEstou != "desaparecidos"){
-        conteudoHTML += `<li class="nav-item space-menu"><a class="nav-item" href="desaparecidos.html">Desaparecidos</a></li>`;
+        conteudoHTML += `<li class="nav-item menu-alinhamento"><a class="nav-item" href="desaparecidos.html">Desaparecidos</a></li>`;
     }
 
     if(ondeEstou != "encontrados"){
-        conteudoHTML += `<li class="nav-item space-menu"><a class="nav-item" href="encontrados.html">Encontrados</a></li>`;
+        conteudoHTML += `<li class="nav-item menu-alinhamento"><a class="nav-item" href="encontrados.html">Encontrados</a></li>`;
     }
 
     if(ondeEstou != "sobre"){
-        conteudoHTML += `<li class="nav-item space-menu"><a class="nav-item" href="sobre.html">Sobre</a></li>`;
-    }       
+        conteudoHTML += `<li class="nav-item menu-alinhamento"><a class="nav-item" href="sobre.html">Sobre</a></li>`;
+    }
     
     conteudoHTML += ultimoItemMenu();
 
@@ -27,17 +27,16 @@ function menuPaginaAtual(ondeEstou){
 
 function ultimoItemMenu(){
     var usuarioCorrente = JSON.parse(sessionStorage.getItem("usuarioCorrente"));
-    let conteudoHTML = "";
-    
+    let conteudoHTML = "";    
 
     if (usuarioCorrente == null) {
-        conteudoHTML += `<li class="nav-item dropdown menor-drop">`;
-        conteudoHTML += `<img src="../../imagens/imagem-perfil.png" width="40" height="30"><br><a class="dropdown-item" id="loginMenu" href="login.html"> Login/<br>Cadastro</a></a>`;
+        conteudoHTML += `<li class="nav-item menu-login">`;
+        conteudoHTML += `<img src="../../imagens/imagem-perfil.png" width="40" height="50"><a class="dropdown-item menu-imagem" id="loginMenu" href="login.html"> Login /<br>Cadastro</a></a>`;
         conteudoHTML += `</li>`;
         conteudoHTML += `</ul>`;  
     } else {
-        conteudoHTML += `<li class="nav-item dropdown menor-drop">`;
-        conteudoHTML += `<a class="nav-item dropdown-toggle menor-size" data-bs-toggle="dropdown" aria-expanded="false">`;
+        conteudoHTML += `<li class="nav-item menu-perfil">`;
+        conteudoHTML += `<a class="nav-item dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">`;
         conteudoHTML += `<img src="../../imagens/imagem-perfil.png" width="40" height="50"><br>Meu Perfil</a>`;
         conteudoHTML += `<ul class="dropdown-menu" id="subMenu">`;
         conteudoHTML += `<li><a class="dropdown-item" href="meu-perfil.html">Meu Perfil</a></li>`;
@@ -45,7 +44,6 @@ function ultimoItemMenu(){
         conteudoHTML += `<li><hr class="dropdown-divider"></li>`;
         conteudoHTML += `<li><a class="dropdown-item" href="cadastro-desaparecido.html">Cadastrar Desaparecido</a></li>`;
         conteudoHTML += `<li><a class="dropdown-item" href="lista-desaparecidos.html">Desaparecidos Cadastrado</a></li>`;
-        conteudoHTML += `<li><a class="dropdown-item" href="chat-e-amigos.html">Chat e Amigos</a></li>`;
         conteudoHTML += `<li><a class="dropdown-item" href="depoimentos.html">Depoimentos</a></li>`;
         conteudoHTML += `<li><a class="dropdown-item" href="#">Configurações</a></li>`;
         conteudoHTML += `</ul>`;
