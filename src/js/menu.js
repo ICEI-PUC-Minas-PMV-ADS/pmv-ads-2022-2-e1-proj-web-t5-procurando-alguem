238318d478480c4edb279f43e642fa1c15eb1815
StehCastell <stehcastell@gmail.com>
2022-12-02 04:15:11 -0300

    ajuste final (?) menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,6 @@
 function menuPaginaAtual(ondeEstou){
     let menuPrincipal = document.getElementById("menuPrincipal");
     let conteudoHTML = "";
-    
-    conteudoHTML += `<ul class="nav">`;
 
     if(ondeEstou != "home-page"){
         conteudoHTML += `<li class="nav-item menu-alinhamento"><a class="nav-item" href="home-page.html">Home Page</a></li>`;
@@ -31,21 +29,21 @@ function ultimoItemMenu(){
 
     if (usuarioCorrente == null) {
         conteudoHTML += `<li class="nav-item menu-login">`;
-        conteudoHTML += `<img src="../../imagens/imagem-perfil.png" width="40" height="50"><a class="dropdown-item menu-imagem" id="loginMenu" href="login.html"> Login /<br>Cadastro</a></a>`;
+        conteudoHTML += `<img src="../../imagens/imagem-perfil.png" width="40" height="50"><a class="dropdown-item menu-imagem" id="loginMenu" href="login.html"> Login /<br>Cadastro</a>`;
         conteudoHTML += `</li>`;
         conteudoHTML += `</ul>`;  
     } else {
         conteudoHTML += `<li class="nav-item menu-perfil">`;
         conteudoHTML += `<a class="nav-item dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">`;
         conteudoHTML += `<img src="../../imagens/imagem-perfil.png" width="40" height="50"><br>Meu Perfil</a>`;
-        conteudoHTML += `<ul class="dropdown-menu" id="subMenu">`;
-        conteudoHTML += `<li><a class="dropdown-item" href="meu-perfil.html">Meu Perfil</a></li>`;
+        conteudoHTML += `<ul class="dropdown-menu sub-menu" id="subMenu">`;
+        conteudoHTML += `<li><a class="dropdown-item item-list-sub-menu" href="meu-perfil.html">Meu Perfil</a></li>`;
         conteudoHTML += `<li><a id="logout" class="dropdown-item" onclick="logoutUsuario()">Logout</a></li>`;
         conteudoHTML += `<li><hr class="dropdown-divider"></li>`;
-        conteudoHTML += `<li><a class="dropdown-item" href="cadastro-desaparecido.html">Cadastrar Desaparecido</a></li>`;
-        conteudoHTML += `<li><a class="dropdown-item" href="lista-desaparecidos.html">Desaparecidos Cadastrado</a></li>`;
-        conteudoHTML += `<li><a class="dropdown-item" href="depoimentos.html">Depoimentos</a></li>`;
-        conteudoHTML += `<li><a class="dropdown-item" href="#">Configurações</a></li>`;
+        conteudoHTML += `<li><a class="dropdown-item item-list-sub-menu" href="cadastro-desaparecido.html">Cadastrar Desaparecido</a></li>`;
+        conteudoHTML += `<li><a class="dropdown-item item-list-sub-menu" href="lista-desaparecidos.html">Desaparecidos Cadastrado</a></li>`;
+        conteudoHTML += `<li><a class="dropdown-item item-list-sub-menu" href="depoimentos.html">Depoimentos</a></li>`;
+        conteudoHTML += `<li><a class="dropdown-item item-list-sub-menu" href="#">Configurações</a></li>`;
         conteudoHTML += `</ul>`;
         conteudoHTML += `</li>`;
         conteudoHTML += `</ul>`;
@@ -57,19 +55,4 @@ function ultimoItemMenu(){
 function logoutUsuario() {
     sessionStorage.setItem('usuarioCorrente', null);
     window.location.href = "login.html";
-}
-
-
-function menuPaginaTeste(){
-    let menuPrincipal = document.getElementById("menuPrincipal");
-    let conteudoHTML = "";
-
-    conteudoHTML += `<li class="nav-item menu-alinhamento"><a class="nav-link" href="home-page.html">Home Page</a></li>`;
-    conteudoHTML += `<li class="nav-item menu-alinhamento"><a class="nav-link" href="desaparecidos.html">Desaparecidos</a></li>`;
-    conteudoHTML += `<li class="nav-item menu-alinhamento"><a class="nav-link" href="encontrados.html">Encontrados</a></li>`;
-    conteudoHTML += `<li class="nav-item menu-alinhamento"><a class="nav-link" href="sobre.html">Sobre</a></li>`;
-
-    conteudoHTML += ultimoItemMenu();
-
-    menuPrincipal.innerHTML = conteudoHTML;
 }
