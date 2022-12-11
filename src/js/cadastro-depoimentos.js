3724fbc453fdd65a8b279c28434d3987c919d965
Pedro Henrique da Silva Santos <pedro.santos.1429140@sga.pucminas.br>
2022-12-11 04:15:31 -0300

    Mais melhorias no Cadastro/Listagem de Depoimentos

--- src/js/cadastro-depoimentos.js
+++ src/js/cadastro-depoimentos.js
@@ -1,95 +1,37 @@
- // declara um conjunto inicial de depoimentos
+ // Declaração de um conjunto inicial de Depoimentos para exemplificação!
 let db_depoimentos = JSON.parse(localStorage.getItem('db_depoimentos')); 
     
     if (!db_depoimentos || db_depoimentos.length == 0) {
         db_depoimentos = [   
         {
             "id": 1,
-            "inputNome": "Leanne Graham",
+            "inputNome": "Pedro Silva",
             "inputDepoimento": "Eu sou um futuro programador!",
              
         },
         {
             "id": 2,
-            "inputNome": "Ervin Howell",
+            "inputNome": "Daniel Rufino",
             "inputDepoimento": "Ano passado perdi um familiar, porém o encontrei aqui neste site!",
             
         },
         {
             "id": 3,
-            "inputNome": "Clementine Bauch",
+            "inputNome": "Stehphanye Castell",
             "inputDepoimento": "Essa plataforma me ajudou a encontrar um conhecido muito querido!",
            
         },
         {
             "id": 4,
-            "inputNome": "Patricia Lebsack",
+            "inputNome": "Andrea Alves",
             "inputDepoimento": "Posso afirmar que esse site já ajudou muitas pessoas!",
             
         },
         {
             "id": 5,
-            "inputNome": "Chelsey Dietrich",
+            "inputNome": "José Durcival",
             "inputDepoimento": "Sinto orgulho dos alunos que fizeram este projeto!",
         }
         ]
         localStorage.setItem('db_depoimentos', JSON.stringify(db_depoimentos));
     }
-
-    
-/*
-// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
-var db = JSON.parse(localStorage.getItem('db_depoimento'));
-if (!db) {
-    db = db_depoimentos_inicial
-};
-
-// Exibe mensagem em um elemento de ID msg
-function displayMessage(msg) {
-    $('#msg').html('<div class="alert alert-warning">' + msg + '</div>');
-}
-
-function inserirDepoimento(depoimento) {
-    // Calcula novo Id a partir do último código existente no array (PODE GERAR ERRO SE A BASE ESTIVER VAZIA)
-    let novoId = 1;
-    if (db.data.length != 0) 
-      novoId = db.data[db.data.length - 1].id + 1;
-    let novoDepoimento = {
-        "id": novoId,
-        "nome": depoimento.nome,
-        "depoimento": depoimento.depoimento,
-
-    };
-
-    // Insere o novo objeto no array
-    db.data.push(novoDepoimento);
-    displayMessage("Depoimento inserido com sucesso");
-
-    // Atualiza os dados no Local Storage
-    localStorage.setItem('db_depoimentos', JSON.stringify(db));
-}
-
-function updateDepoimento(id, depoimento) {
-    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
-    let index = db.data.map(obj => obj.id).indexOf(id);
-
-    // Altera os dados do objeto no array
-    db.data[index].nome = depoimento.nome,
-    db.data[index].email = depoimento.email,
-    db.data[index].depoimento = depoimento.depoimento,
-
-    displayMessage("Depoimento alterado com sucesso");
-
-    // Atualiza os dados no Local Storage
-    localStorage.setItem('db_depoimento', JSON.stringify(db));
-}
-
-function deleteDepoimento(id) {    
-    // Filtra o array removendo o elemento com o id passado
-    db.data = db.data.filter(function (element) { return element.id != id });
-
-    displayMessage("Depoimento removido com sucesso");
-
-    // Atualiza os dados no Local Storage
-    localStorage.setItem('db_depoimento', JSON.stringify(db));
-} */
